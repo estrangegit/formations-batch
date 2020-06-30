1137ef2dcb3175cd9f182c7ffc5070426df24a74
estrangegit <etienne.estrangin@gmail.com>
2020-07-01 01:30:38 +0200

    Chargement des séances: suppression d'un logger inutile

--- src/main/java/com/zaroumia/batch/deciders/SeancesStepDecider.java
+++ src/main/java/com/zaroumia/batch/deciders/SeancesStepDecider.java
@@ -1,15 +1,11 @@
 package com.zaroumia.batch.deciders;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.job.flow.FlowExecutionStatus;
 import org.springframework.batch.core.job.flow.JobExecutionDecider;
 import org.springframework.util.StringUtils;
 
 public class SeancesStepDecider implements JobExecutionDecider {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(SeancesStepDecider.class);
 
     @Override
     public FlowExecutionStatus decide(JobExecution jobExecution,
