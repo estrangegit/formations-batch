85845a334c4eef85ddedceeb760cd6bb2aa88b56
estrangegit <etienne.estrangin@gmail.com>
2020-07-04 15:09:40 +0200

    Test de l'application: chargement des formations

---
+++ src/test/java/com/estrange/batch/BaseTest.java
@@ -0,0 +1,34 @@
+package com.estrange.batch;
+
+import org.junit.runner.RunWith;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@JdbcTest
+@SpringBatchTest
+@ContextConfiguration(classes = ConfigurationForTest.class)
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
+public abstract class BaseTest {
+
+    @Autowired
+    protected JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    protected JdbcTemplate jdbcTemplate;
+
+    @MockBean
+    protected freemarker.template.Configuration conf;
+
+    @MockBean
+    protected JavaMailSender javaMailSender;
+}
--- src/test/java/com/estrange/batch/ChargementFormationsStepConfigTest.java
+++ src/test/java/com/estrange/batch/ChargementFormationsStepConfigTest.java
@@ -7,20 +7,20 @@ import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 
-class ChargementFormateursStepConfigTest extends BaseTest {
+class ChargementFormationsStepConfigTest extends BaseTest {
 
     @Test
-    void shouldLoadFormateursWithSuccess() {
+    void shouldLoadFormationsWithSuccess() {
         JobParameters jobParameters = new JobParametersBuilder()
-                .addString("formateursFile", "classpath:inputs/formateursFile.csv")
+                .addString("formationsFile", "classpath:inputs/formationsFile.xml")
                 .toJobParameters();
 
         JobExecution result =
-                jobLauncherTestUtils.launchStep("chargementFormateursStep", jobParameters);
+                jobLauncherTestUtils.launchStep("chargementFormationsStep", jobParameters);
         assertThat(result.getExitStatus()).isEqualTo(ExitStatus.COMPLETED);
 
         Integer count =
-                jdbcTemplate.queryForObject("SELECT COUNT(*) FROM formateurs;", Integer.class);
-        assertThat(count).isEqualTo(16);
+                jdbcTemplate.queryForObject("SELECT COUNT(*) FROM formations;", Integer.class);
+        assertThat(count).isEqualTo(4);
     }
 }
